8c53cd5dc7b175c1d7230bf08a397151956e6302
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-09-29 11:50:22 -0300

    Add filterirng by year

--- app/scr/akinator.pl
+++ app/scr/akinator.pl
@@ -1,2 +1,9 @@
 
---akinator
+%akinator
+
+
+%Filtering by year
+year(Year):-year(_, Year,_,_,_,_,_).
+year_genre(Year,Genre):-year(_, Year,Genre,_,_,_,_).
+year_actor(Year):-year(_, Year,_,Actor,Actor,Actor,_).
+year_hanking(Year):-year(_, Year,_,_,_,_,Hanking).
