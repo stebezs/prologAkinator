0f35e7f90aabbf25f73729a790dee669620e0cd9
Stefania Bezerra da Silva <stefaniabezerra95@gmail.com>
2017-10-02 09:30:26 -0300

    Others filters

--- src/akinator.pl
+++ src/akinator.pl
@@ -46,3 +46,56 @@ actor(_,_,_,Actor,_).
 actor_year(_,Year,_,Actor,_).
 actor_genre(_,_,Genre,Actor,_).
 actor_ranking(_,_,_,Actor,Ranking).
+
+movie(2009,action).
+movie(1997,drama).
+movie(2015,action).
+movie(2015,action).
+movie(2012,action).
+movie(2009,action).
+movie(2015,action).
+movie(2011,animation).
+movie(2013,fantasy).
+movie(2017,action).
+movie(2017,action).
+movie(2013,animation).
+movie(2015,action).
+movie(2016,action).
+movie(2011,action).
+movie(2003,action).
+movie(2012,action).
+movie(2014,action).
+movie(2012,action).
+movie(2010,animation).
+movie(2006,action).
+movie(2016,action).
+movie(2011,action).
+movie(1993,fantasy).
+movie(2016,animation).
+
+
+movie(['Sam Worthington', 'Zoe Saldana', 'Sigourney Weaver'], 1).
+movie(['Leonardo DiCaprio', 'Kate Winslet', 'Billy Zane'], 2).
+movie(['Harrison Ford', 'Mark Hamill', 'Carrie Fisher'], 3).
+movie(['Chris Pratt', 'Bryce Dallas Howard', '	Irrfan Khan'], 4).
+movie(['Robert Downey Jr.', 'Chris Evans', ' Scarlett Johansson'], 5).
+movie(['Vin Diesel', 'Paul Walker', 'Jason Statham'], 6).
+movie(['Robert Downey Jr.', 'Chris Hemsworth', 'Mark Ruffalo'], 7).
+movie(['Daniel Radcliffe', 'Emma Watson', 'Rupert Grint'], 8).
+movie(['Idina Menzel', 'Kristen Bell',  'Jonathan Groff '], 9).
+movie(['Emma Watson', 'Dan Steves', 'Luke Evans'], 10).
+movie(['Vin Diesel', 'Jason Statham', 'Dwayne Johnson'], 11).
+movie(['Robert Downey Jr.', 'Guy Pearce', 'Gwyneth Paltrow'], 12).
+movie(['Sandra Bullock', 'Jon Hamm', 'Michael Keaton'], 13).
+movie(['Chris Evans', 'Robert Downey Jr.', 'Scarlett Johansson'], 14).
+movie(['Shia LaBeouf', 'Rosie Huntington-Whiteley', 'Tyrese Gibson'], 15).
+movie(['Elijah Wood', 'Ian McKellen', 'Viggo Mortensen'], 16).
+movie(['Daniel Craig', 'Javier Bardem', 'Ralph Fiennes'], 17).
+movie(['Don Murphy', 'Tom DeSanto', 'Lorenzo di Bonaventura'], 18).
+movie(['Christian Bale', 'Michael Caine', 'Gary Oldman'], 19).
+movie(['Tom Hanks', 'Tim Allen', 'Joan Cusack'], 20).
+movie(['Johnny Depp', 'Orlando Bloom', 'Keira Knightley'], 21).
+movie(['Felicity Jones', 'Diego Luna', 'Ben Mendelsohn'], 22).
+movie(['Johnny Depp', 'Penélope Cruz', 'Ian McShane'], 23).
+movie(['Sam Neill', 'Laura Dern', 'Jeff Goldblum'], 24).
+movie(['Ellen DeGeneres', 'Albert Brooks', 'Hayden Rolence'], 25).
